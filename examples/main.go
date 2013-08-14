bbaf5bbe3fcfb5d491936e6f35e33d67a7942278
athom <athom@126.com>
2013-08-14 11:02:08 +0800

    add change email demo

--- examples/main.go
+++ examples/main.go
@@ -10,6 +10,9 @@ func main() {
 	apiKey := os.Getenv("MAILCHIMP_API_KEY")
 	listId := os.Getenv("MAILCHIMP_LIST_ID")
 
+	apiKey = "4bad4d3a5985f2e1a5a1c6660f0a86bf-us4"
+	listId = "0a5644dbaf"
+
 	lemur.SetApiKey(apiKey)
 	//err := lemur.Ping()
 	//err := lemur.AccountDetails(nil)
@@ -19,28 +22,35 @@ func main() {
 	//err := lemur.MergeVarAdd( "ORGNAME", "OrganizationName", nil)
 	//err := lemur.MergeVarDel( "ORGNAME")
 
-	err := lemur.ListUpdateMember(
-		"frankyue1019@gmail.com",
-		map[string]string{
-			"FNAME": "Famm ",
-			"LNAME": "Ge",
-		},
-	)
-
-	//err := lemur.ListSubscribe(
-	//"yeerkunth+1309@gmail.com",
+	//err := lemur.ListUpdateMember(
+	//"frankyue1019@gmail.com",
 	//map[string]string{
-	//"FNAME": "Yeer",
-	//"LNAME": "Kunnnnn",
+	//"FNAME": "Famm ",
+	//"LNAME": "Ge",
 	//},
 	//)
+
+	err := lemur.ListSubscribe(
+		"yeerkunth+1313@gmail.com",
+		map[string]string{
+			"FNAME": "Yeer",
+			"LNAME": "Kunnnnn",
+		},
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = lemur.ListUpdateMember(
+		"yeerkunth+1313@gmail.com",
+		map[string]string{
+			"new-email": "yeerkunth+1344@gmail.com",
+		},
+	)
 
 	//err := lemur.ListUnsubscribe(
 	//"yeerkunth+1309@gmail.com",
 	//)
 
-	if err != nil {
-		fmt.Println(err)
-	}
 	return
 }
